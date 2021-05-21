e61a49d3255e59130caec2d2c6f399542a71b967
Felipe Costa <felipecostati@gmail.com>
2021-05-21 17:03:14 -0300

    seta back

--- frontend/src/pages/AddSale/index.tsx
+++ frontend/src/pages/AddSale/index.tsx
@@ -1,6 +1,7 @@
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 import { Link } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 
 
 const AddSale = () => {
@@ -11,13 +12,24 @@ const AddSale = () => {
       <div className="container">
         <div className="jumbotron">
           <h1 className="display-4">
+            <Link className="fs-5 me-3 my-auto" to="/dashboard">
+              <FaArrowLeft />
+            </Link>
             Add Sale
           </h1>
           <hr />
-          <Link className="btn btn-primary mr-5 btn-lg" to="/dashboard">
+          <div>
+            <select className="form-select" aria-label="Default select example">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
             Salvar
           </Link>
-          <Link className="btn btn-danger btn-lg" to="/dashboard">
+          <Link className="btn btn-danger ms-5 btn-lg" to="/dashboard">
             Cancel
           </Link>
         </div>
